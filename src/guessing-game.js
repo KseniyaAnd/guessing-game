354ff9a68437604337720dde17b93386bbd33dd2
KseniyaAnd <kseniya.andrusenko72@gmail.com>
2023-09-10 12:56:38 +0300

    init: guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,20 +1,26 @@
 class GuessingGame {
-    constructor() {}
+    constructor() {
+        this.min = 0;
+        this.max = 0;
+        this.candidate = 0;
+    }
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.candidate = Math.floor((this.min + this.max) / 2);
+        return this.candidate;
     }
 
     lower() {
-
+        this.max = this.candidate - 1;
     }
 
     greater() {
-
+        this.min = this.candidate + 1;
     }
 }
 
